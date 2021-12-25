b9b0896768a9762a9c62c7cede63d5b43dcc6890
DNC <dnc@DNCs-MacBook-Pro-Andrey.local>
2021-12-25 21:04:34 +0200

    Fixed overflow

--- src/modules/MainArea/index.jsx
+++ src/modules/MainArea/index.jsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import SideBarContext from "contexts/SidebarContext";
 import HomePage from "modules/HomePage";
+import { useMediaQuery } from "react-responsive";
 import { Routes, Route } from "react-router-dom";
 
 import routesByName from "constants/routesByName";
@@ -12,22 +13,31 @@ import Whitepaper from "modules/Whitepaper";
 
 import s from "./MainArea.module.scss";
 
-const MainArea = ({ sideBarOpen, toggleSideBar }) => (
-  <div className={clsx(s.mainArea, sideBarOpen && "overflowHidden")}>
-    <Routes>
-      <Route path={routesByName.home} element={<HomePage />} exact />
-      <Route path={routesByName.community} element={<Community />} />
-      <Route path={routesByName.whitepaper} element={<Whitepaper />} />
-      <Route path={routesByName.developers} element={<Developers />} />
-      <Route path={routesByName.careers} element={<Careers />} />
-      <Route path={routesByName.events} element={<Events />} />
-    </Routes>
+const MainArea = ({ sideBarOpen, toggleSideBar }) => {
+  const maxWidth1200 = useMediaQuery({ maxWidth: 1200 });
+
+  return (
     <div
-      className={clsx(s.rootShadow, sideBarOpen && s.shadowActive)}
-      onClick={toggleSideBar}
-    />
-  </div>
-);
+      className={clsx(
+        s.mainArea,
+        sideBarOpen && maxWidth1200 && "overflowHidden"
+      )}
+    >
+      <Routes>
+        <Route path={routesByName.home} element={<HomePage />} exact />
+        <Route path={routesByName.community} element={<Community />} />
+        <Route path={routesByName.whitepaper} element={<Whitepaper />} />
+        <Route path={routesByName.developers} element={<Developers />} />
+        <Route path={routesByName.careers} element={<Careers />} />
+        <Route path={routesByName.events} element={<Events />} />
+      </Routes>
+      <div
+        className={clsx(s.rootShadow, sideBarOpen && s.shadowActive)}
+        onClick={toggleSideBar}
+      />
+    </div>
+  );
+};
 
 const MainAreaWithContext = () => (
   <SideBarContext.Consumer>
